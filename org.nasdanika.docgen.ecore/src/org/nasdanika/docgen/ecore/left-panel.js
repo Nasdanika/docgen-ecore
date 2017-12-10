96ce126dd42f1622f094f9d4ff19a55bacdd1879
Pavel <Pavel.Vlasov@nasdanika.org>
2017-12-10 14:33:46 -0500

    Removed search functionality.

--- org.nasdanika.docgen.ecore/src/org/nasdanika/docgen/ecore/left-panel.js
+++ org.nasdanika.docgen.ecore/src/org/nasdanika/docgen/ecore/left-panel.js
@@ -22,56 +22,7 @@ define(['jquery', 'knockout', 'q', './../../toc.js', './../jstree/jstree.js', 'd
 		}
 	});
 
-	var leftOverlay = jQuery("#left-overlay");
-	
-	var searchContainer = doc.getElementById('search-container');
-	
-	if (searchContainer) {
-		ko.applyBindings({
-			
-			query: ko.observable(),
-			error: ko.observable(),
-			searchKeyPress: function(data, event) {
-			    if (event.which == 13) {
-			        this.search();
-			    } else {
-			    	return true;
-			    }			
-			},
-			search: function() {
-				leftOverlay.height(leftOverlay.parent().height());
-				leftOverlay.width(leftOverlay.parent().width());
-				leftOverlay.css("display", "block");
-				this.error(undefined);
-				this.results(undefined);
-				jQuery.ajax("doc/search", {
-					data: {
-						query: this.query
-					},
-					dataType: "json",
-					success: function(data, textStatus, jqXHR) {
-						if (data.length>0) {
-							this.results(data);
-						} else {
-							this.error("No results");
-						}					
-					}.bind(this),
-					error: function(jqXHR, textStatus, errorThrown) {
-						this.error("Search error: "+errorThrown);				
-					}.bind(this),
-					complete: function() {
-						leftOverlay.css("display", "none");						
-					}
-				});
-			},
-			
-			results: ko.observable()
-			
-		}, searchContainer);
-	} else {
-		console.error("Search container not found");
-	}
-	
+	var leftOverlay = jQuery("#left-overlay");		
 	var jTocSearch = jQuery('#toc-search');
 	var to = false;	
 	jTocSearch.keyup(function () {
